0503592f58139d183c08677a137412c7feee23f3
CipherCrow <jonastorquatro@gmail.com>
2026-01-02 17:49:33 -0300

    adicionando card criar evento

--- src/components/formularioDeEvento/index.jsx
+++ src/components/formularioDeEvento/index.jsx
@@ -1,22 +1,52 @@
 import "./styles.css"
 
+function CampoDeFormulario({children}){
+    return(
+        <fieldset className="campoForm">
+            {children}
+        </fieldset>
+    )
+}
+
+function Label({children,htmlFor}){
+    return(
+        <label htmlFor={htmlFor}>
+            {children}
+        </label>
+    )
+}
+
+function CampoDeEntrada({type,id,placeholder}){
+    return(
+        <input type={type} id={id} placeholder={placeholder}/>
+    )
+}
+
 export default function FormularioDeEvento(){
   return(
     <form className="formEvento">
       <h2>Preencha para criar um evento:</h2>
-      <fieldset>
-          <label htmlFor="nomeEvento">Qual o nome do evento?</label>
-          <input type="text" id="nomeEvento" />
-      </fieldset>
-      <fieldset>
-          <label htmlFor="dataEvento">Data do Evento</label>
-          <input type="date" id="dataEvento" />
-      </fieldset>
-      <fieldset>
-          <label htmlFor="temaEvento">Tema do Evento</label>
-          <input type="text" id="nomeEvento" />
-      </fieldset>
-      <input type="submit" value="Criar Evento" />
+      <div className="formEvento_campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nomeEvento">Qual o nome do evento?</Label>
+          <CampoDeEntrada type="text" id="nomeEvento" placeholder="Summer Dev Hits"/>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do Evento</Label>
+          <CampoDeEntrada type="date" id="dataEvento" placeholder="XX/XX/XXXX"/>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="temaEvento">Tema do Evento</Label>
+          <select name="" id="">
+            <option value="">Tecnologia</option>
+            <option value="">Saúde</option>
+            <option value="">Educação</option>
+          </select>
+        </CampoDeFormulario>
+      </div>
+      <div className="formEvento_acoes">
+        <input type="submit" value="Criar Evento" />
+      </div>
     </form>
   ) 
 }
