47d57a8f1547dc4d8ee6a191ae5d3ab191bd2a96
CipherCrow <jonastorquatro@gmail.com>
2026-01-03 15:35:04 -0300

    ultimos ajustes no estilo do form

--- src/components/formularioDeEvento/index.jsx
+++ src/components/formularioDeEvento/index.jsx
@@ -24,23 +24,28 @@ function CampoDeEntrada({type,id,placeholder}){
 
 export default function FormularioDeEvento({temas}){
   return(
-    <form className="formEvento">
+    <form className="formEvento" action={postMessage}>
       <h2>Preencha para criar um evento:</h2>
       <div className="formEvento_campos">
         <CampoDeFormulario>
-          <Label htmlFor="nomeEvento">Qual o nome do evento?</Label>
+          <Label htmlFor="nomeEvento">Qual o nome do Evento?</Label>
           <CampoDeEntrada type="text" id="nomeEvento" placeholder="Summer Dev Hits"/>
         </CampoDeFormulario>
         <CampoDeFormulario>
+          <Label htmlFor="imagemEvento">Qual o enderço da imagem de capa do Evento?</Label>
+          <CampoDeEntrada type="text" id="imagemEvento" placeholder="https://minhaimagemfavorita.com.br/imagem.png"/>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
           <Label htmlFor="dataEvento">Data do Evento</Label>
-          <CampoDeEntrada type="date" id="dataEvento" placeholder="XX/XX/XXXX"/>
+          <CampoDeEntrada type="date" id="dataEvento"/>
         </CampoDeFormulario>
         <CampoDeFormulario>
           <Label htmlFor="temaEvento">Tema do Evento</Label>
-          <select name="" id="">
+          <select id="temaEvento" defaultValue="">
+            <option value="" disabled >Selecione uma Opção</option>
             {
               temas.map((tema) => (
-                <option key={tema.id} value={tema.nome}>{tema.nome}</option>
+                <option key={tema.id} value={tema.id}>{tema.nome}</option>
               ))
             }
           </select>
